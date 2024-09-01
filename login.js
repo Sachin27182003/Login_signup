f3c527ac6278e7f76e865825e3f3ac70cf72c02a
Sachin Kumar <155556062+Sachin27182003@users.noreply.github.com>
2024-09-01 20:37:51 +0530

    update 1.0

--- login.js
+++ login.js
@@ -4,6 +4,20 @@
   });
 
 
+  function getdata(){
+
+    const data = JSON.parse(localStorage.getItem("data"))  || {"dataList": []};
+    console.log(data);
+    return data;
+  }
+
+  function addToLocalStorage(info){
+    const data = getdata();
+
+
+    data.dataList.push(info);
+    localStorage.setItem("data", JSON.stringify(data));
+  }
 
 
   document.addEventListener("DOMContentLoaded", () => {
@@ -18,7 +32,9 @@
    const validateForm = () => {
 
     const email_test = /^(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/;
-    const password_test = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]{8,}$/;
+    const password_test = /^(?=.*[a-z])(?=.*[A-Z])(?=.*0-9)[a-zA-Z0-9]{8,}$/;
+
+    
 
     if(name_up.value === '' ){
          alert("name can't be blank");
@@ -32,7 +48,7 @@
     } else if (password_up.value === '' ){
          alert("password can't be blank");
          return false;
-    } else if (password_test.test(password_up)){
+    } else if (password_test.test(password_up.value)){
         alert("password must contain 8 characters incuding atleast a digit an aphabet combination of upper and lower case")
         return false;
     } else {
@@ -42,7 +58,7 @@
    }
 
    password_up.addEventListener("keydown", (event) => {
-    if(event.key === "enter" && validateForm()){
+    if(event.key === "Enter" && validateForm()){
         login(); 
     }
 })
@@ -54,11 +70,25 @@
   })
 
 
+  function login(){
+      const name__up = name_up.value;
+      const email__up = email_up.value; 
+      const password__up = password_up.value;
+
+
+    addToLocalStorage({Name: name__up, Email: email__up, Password: password__up});
+        name_up.value = '';
+        email_up.value = '';
+        password_up.value = '';
+ }
+
+
+
+
+
+
   })
 
 
 
-  function login(){
-      alert("Login successfull");
-
-  }
+  
